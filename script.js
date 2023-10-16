103de63b0c405731cfb5f95e94633d51a07caa26
xSYl25 <142290884+xSyl25@users.noreply.github.com>
2023-10-16 03:55:51 -0600

    add result and player variables, and play function

--- script.js
+++ script.js
@@ -9,8 +9,46 @@
 // loop game 5 times
 // return a winner
 
+let playerChoice = prompt("Choose! Rock, paper, scissors!");
+
+console.log(playerChoice);
+
 function getComputerChoice() {
-    let options = ['Rock', 'Paper', 'Scissors'];
-    let choice = Math.floor(Math.random() * options.length)
+    let options = ["Rock", "Paper", "Scissors"];
+    let choice = Math.floor(Math.random() * options.length);
     return options[choice];
 }
+
+console.log(getComputerChoice())
+
+const win = "You win!";
+const lose = "You lose!";
+const tie = "It's a tie!";
+
+function play() {
+    if(playerChoice === getComputerChoice()) {
+        return tie;
+    } else if(playerChoice === "Rock") {
+        if(getComputerChoice() === "Paper") {
+            return lose;
+        } else if(getComputerChoice() === "Scissors") {
+            return win;
+        }
+    } else if(playerChoice === "Paper") {
+        if(getComputerChoice() === "Scissors") {
+            return lose;
+        } else if(getComputerChoice() === "Rock") {
+            return win;
+        }
+    } else if(playerChoice === "Scissors") {
+        if(getComputerChoice() === "Rock") {
+            return lose;
+        } else if(getComputerChoice() === "Paper") {
+            return win;
+        }
+    } else {
+        return "Invalid choice."
+    }
+}
+
+console.log(play())
